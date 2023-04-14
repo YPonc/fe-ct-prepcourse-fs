9a4480a67d5cb6db05465dbecca3203571a7fb3f
YPonc <york_uriarte_10@hotmail.com>
2023-04-14 01:03:16 -0500

    Update jss.js

--- M03 JavaScript Fundamentos/jss.js
+++ M03 JavaScript Fundamentos/jss.js
@@ -1,3 +1,15 @@
 var cajon1 = "Gola";
 
 console.log(cajon1);
+
+var cajon1 = "Gola";
+
+console.log(cajon1);
+
+var cajon1 = "Gola";
+
+console.log(cajon1);
+
+var cajon1 = "Gola";
+
+console.log(cajon1);
